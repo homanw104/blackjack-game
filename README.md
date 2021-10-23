36ab0160971a711e9d76e5b87bca83180b9586d9
Homan <homanwang@hotmail.com>
2021-10-23 21:05:34 +0800

    Update UI & add onClick events

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Blackjack
 
 A blackjack game designed using OOAD practices.
+
+## UI Framework
+
+The project UI is built with JavaFX. To set up a development environment,
+check [Create a new JavaFX project](https://jetbrains.com/help/idea/javafx.html)
+in IntelliJ IDEA Edu for reference.
--- src/main/java/world/homans/blackjack/BlackjackController.java
+++ src/main/java/world/homans/blackjack/BlackjackController.java
@@ -1,15 +1,87 @@
 package world.homans.blackjack;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 
 public class BlackjackController {
 
-    @FXML
-    private Label welcomeText;
+    Player player = new Player();
+    Dealer dealer;
+    Deck deck;
+    Game game;
 
+    /**
+     * Player adds a $100 value chip to the bet.
+     */
     @FXML
-    protected void onNewGameButtonClick() {
-        welcomeText.setText("Welcome to Blackjack Game!");
+    protected void onBet100ChipClicked() {
+//        game.increaseBet(new Chip(100));
+        System.out.println("Bet 100");
+    }
+
+    /**
+     * Player adds a $500 value chip to the bet.
+     */
+    @FXML
+    protected void onBet500ChipClicked() {
+//        game.increaseBet(new Chip(500));
+        System.out.println("Bet 500");
+    }
+
+    /**
+     * Player adds a $1000 value chip to the bet.
+     */
+    @FXML
+    protected void onBet1000ChipClicked() {
+//        game.increaseBet(new Chip(1000));
+        System.out.println("Bet 1000");
+    }
+
+    /**
+     * Player adds a $5000 value chip to the bet.
+     */
+    @FXML
+    protected void onBet5000ChipClicked() {
+//        game.increaseBet(new Chip(5000));
+        System.out.println("Bet 5000");
+    }
+
+    /**
+     * Cancel the bet and return all chips.
+     */
+    @FXML
+    protected void onCancelButtonClick() {
+
+    }
+
+    /**
+     * Deal the bet (chips) and start the game.
+     */
+    @FXML
+    protected void onDealButtonClick() {
+
+    }
+
+    /**
+     * Player takes no more cards.
+     */
+    @FXML
+    protected void onStandButtonClick() {
+
+    }
+
+    /**
+     * Player takes another card.
+     */
+    @FXML
+    protected void onHitButtonClick() {
+
+    }
+
+    /**
+     * Player double the bet and take exactly one more card.
+     */
+    @FXML
+    protected void onDoubleButtonClick() {
+
     }
 }
